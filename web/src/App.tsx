6861d8b820f8d5f115d48459dca2b4dfd7170aac
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2020-08-03 11:41:48 -0300

    Inicial commit

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// JSX - JavaScript + XML
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+
+    </div>
+  );
+}
+
+export default App;
